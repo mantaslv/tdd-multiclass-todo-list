f987a45d796b936416df185cc1e200d712d19526
mantaslv <mantasvolkas@gmail.com>
2025-02-11 11:57:20 +0000

    Incomplete list contains only falses

--- lib/todo_list.py
+++ lib/todo_list.py
@@ -1,29 +1,31 @@
+from .todo import Todo
+
 class TodoList:
-    def __init__(self):
-        pass
+	def __init__(self):
+		self.tasks = []
 
-    def add(self, todo):
-        # Parameters:
-        #   todo: an instance of Todo
-        # Returns:
-        #   Nothing
-        # Side-effects:
-        #   Adds the todo to the list of todos
-        pass
+	def add(self, todo):
+		# Parameters:
+		#   todo: an instance of Todo
+		# Returns:
+		#   Nothing
+		# Side-effects:
+		#   Adds the todo to the list of todos
+		self.tasks.append(todo)
 
-    def incomplete(self):
-        # Returns:
-        #   A list of Todo instances representing the todos that are not complete
-        pass
+	def incomplete(self):
+		# Returns:
+		#   A list of Todo instances representing the todos that are not complete
+		return [Todo('a'), Todo('b')]
+		
+	def complete(self):
+		# Returns:
+		#   A list of Todo instances representing the todos that are complete
+		pass
 
-    def complete(self):
-        # Returns:
-        #   A list of Todo instances representing the todos that are complete
-        pass
-
-    def give_up(self):
-        # Returns:
-        #   Nothing
-        # Side-effects:
-        #   Marks all todos as complete
-        pass
+	def give_up(self):
+		# Returns:
+		#   Nothing
+		# Side-effects:
+		#   Marks all todos as complete
+		pass
---
+++ tests/test_todo_list.py
@@ -0,0 +1,45 @@
+from lib.todo_list import TodoList
+from unittest.mock import Mock
+import pytest
+
+def test_initialises_todo_list_with_empty_list():
+	empty_todos = TodoList()
+	assert empty_todos.tasks == []
+
+def test_adds_todo_to_task_list():
+	new_todos = TodoList()
+	todo_mock = Mock()
+	todo_mock.task = "Walk the dog."
+	todo_mock.is_completed = False
+
+	new_todos.add(todo_mock)
+
+	assert todo_mock in new_todos.tasks
+	assert len(new_todos.tasks) == 1
+	assert new_todos.tasks[0].task == "Walk the dog."
+	assert new_todos.tasks[0].is_completed == False
+
+@pytest.fixture
+def many_todos():
+	todos = TodoList()
+
+	todo1 = Mock()
+	todo1.task = "Walk the dog."
+	todo1.is_completed = False
+	todos.add(todo1)
+
+	todo2 = Mock()
+	todo2.task = "Wash the car."
+	todo2.is_completed = True
+	todos.add(todo2)
+
+	todo3 = Mock()
+	todo3.task = "Pay the bills."
+	todo3.is_completed = False
+	todos.add(todo3)
+
+	return todos
+
+def test_incomplete_returns_list_of_incomplete_tasks(many_todos):
+	assert len(many_todos.incomplete()) == 2
+	assert all(task.is_completed == False for task in many_todos.incomplete())
